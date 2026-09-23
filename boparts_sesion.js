9cb9a9a30afb39a649bde6ff1a4e996050359a7c
osunarodolfo31-ui <osunarodolfo31@gmail.com>
2026-09-22 22:23:21 -0400

    Add files via upload

--- boparts_sesion.js
+++ boparts_sesion.js
@@ -208,7 +208,7 @@ function revisarSesion(alTener) {
    version.json dice cual es la buena, y si no coinciden aparece una barra con un boton que borra la
    cache y recarga. Nadie tiene que saber que es "borrar cache".
    Ademas, todo enlace entre pantallas lleva ?v=BUILD: moverse por el menu ya no trae copias viejas. */
-var BUILD = '2026-09-22.4';
+var BUILD = '2026-09-22.5';
 (function() {
   var intentos = 0;
   function sellarEnlaces() {
